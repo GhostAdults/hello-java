57dd94b7f4eb19d2ae52a36529e13ea3f0f28f7e
GhostAdults <ssy4240@sina.com>
2021-04-05 14:20:51 +0800

    test3

--- demo2/src/main/java/com/example/demo2/controller/test.java
+++ demo2/src/main/java/com/example/demo2/controller/test.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @Autowired
     @RequestMapping("/")
-    public String  Test(){
+    public String Test() {
         System.out.println("demo2-gitest");
+
         return "successed";
     }
 }
